27a32cbb4f788e9732d279df4baa6c4b791e3f15
Marco <marcodefalco2017@libero.it>
2026-02-28 15:22:12 +0100

    clerk

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,34 +1,38 @@
 import { Suspense } from "react";
 
-import Stats from "@/app/_components/dashboard/Stats";
-import OrdersActivityWrapper from "@/app/_components/dashboard/OrdersActivityWrapper";
-import BestSellerChartWrapper from "@/app/_components/dashboard/BestSellerChartWrapper";
-import ChartsWrapper from "@/app/_components/dashboard/ChartsWrapper";
-import RevenueChartWrapper from "@/app/_components/dashboard/RevenueChartWrapper";
-import OrdersChartWrapper from "@/app/_components/dashboard/OrdersChartWrapper";
+import Stats from "@/app/_components/dashboard/stats/Stats";
+import OrdersActivityWrapper from "@/app/_components/dashboard/ordersActivity/OrdersActivityWrapper";
+import BestSellerChartWrapper from "@/app/_components/dashboard/bestSellers/BestSellerChartWrapper";
+import ChartsResolver from "@/app/_components/dashboard/charts/ChartsResolver";
 
 import DialogContextProvider from "@/app/_contexts/DialogContext";
 import CustomDialogWrapper from "@/app/_components/ui/dialog/CustomDialogWrapper";
 
 import {
   BestSellersSkeleton,
+  ChartsSkeleton,
   OrdersActivitySkeleton,
-  OrdersAreaChartSkeleton,
-  RevenueChartSkeleton,
   StatsSkeleton,
 } from "@/app/_components/ui/Skeletons";
+import { DateRangeParams } from "@/app/_lib/definitions";
 
 export const metadata = {
   title: "Dashboard",
 };
 
-export default function Page() {
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: Promise<DateRangeParams>;
+}) {
+  const { dateRange = "last-7-days" } = await searchParams;
+
   return (
     <>
       <div className="grid grid-cols-2 grid-rows-[auto_22rem_auto_1fr] gap-x-8 gap-y-12 md:grid-cols-[1fr_1fr] md:grid-rows-[auto_28rem_auto_1fr] lg:grid-cols-[1fr_1fr] lg:grid-rows-[auto_auto_1fr]">
         <div className="col-span-full">
-          <Suspense fallback={<StatsSkeleton />}>
-            <Stats />
+          <Suspense fallback={<StatsSkeleton />} key={dateRange}>
+            <Stats dateRange={dateRange} />
           </Suspense>
         </div>
 
@@ -44,18 +48,9 @@ export default function Page() {
           <CustomDialogWrapper />
         </DialogContextProvider>
 
-        <ChartsWrapper
-          revenueChart={
-            <Suspense fallback={<RevenueChartSkeleton />}>
-              <RevenueChartWrapper />
-            </Suspense>
-          }
-          ordersChart={
-            <Suspense fallback={<OrdersAreaChartSkeleton />}>
-              <OrdersChartWrapper />
-            </Suspense>
-          }
-        />
+        <Suspense fallback={<ChartsSkeleton />}>
+          <ChartsResolver dateRange={dateRange} />
+        </Suspense>
       </div>
     </>
   );
